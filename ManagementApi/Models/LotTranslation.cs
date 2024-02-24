1d97fe9d4c17f0b5379c99dc4b2c68d5c58749be
Michael Obikwee <michael.obikwee@novafori.com>
2024-02-24 11:24:08 +0000

    Introduced new models AuctionEventItemResponse and LotTranslation

---
+++ ManagementApi/Models/LotTranslation.cs
@@ -0,0 +1,19 @@
+﻿namespace ManagementApi.Models
+{
+    public class LotTranslation
+    {
+        public string? ArtistMaker { get; set; }
+        public string? ConditionReport { get; set; }
+        public string? Description { get; set; }
+        public string? Engraved {  get; set; }
+        public string? Exhibited { get; set; }
+        public string? ExtraInformation { get; set; }
+        public string? LanguageCode { get; set; } = "en-US";
+        public string? Literature { get; set; }
+        public string? LotEssay { get; set; }
+        public string? PostLotText { get; set; }
+        public string? PreLotText { get; set; }
+        public string? Provenance { get; set; }
+        public string? Title { get; set; }
+    }
+}
