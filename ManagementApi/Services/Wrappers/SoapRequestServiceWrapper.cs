25eb2e05e6ecd5f86e9aa62ddd0e830ba03430bd
Michael O <michaelobikwee@yahoo.co.uk>
2024-03-09 17:32:45 +0000

    Added interface and concrete class for SoapRequestServiceWrapper

---
+++ ManagementApi/Services/Wrappers/SoapRequestServiceWrapper.cs
@@ -0,0 +1,27 @@
+﻿namespace ManagementApi.Services.Wrappers
+{
+    using ManagementApi.Factories;
+    using ManagementApi.Models;
+    using ManagementApi.Services.Interfaces;
+    using System.Data.SqlClient;
+    using System.Threading.Tasks;
+
+    public class SoapRequestServiceWrapper : ISoapRequestServiceWrapper
+    {
+        private readonly SqlConnection _sqlConnection;
+
+        public SoapRequestServiceWrapper(SqlConnection sqlConnection)
+        {
+            _sqlConnection = sqlConnection;
+        }
+
+        public async Task<string> CreateSOAPRequest(SOAPRequestServiceRequest request)
+        {
+            var wrapper = new SqlConnectionWrapper(_sqlConnection);
+            var soapFactory = new SOAPRequestServiceFactory(wrapper);
+            var soapRequestService = soapFactory.Create(request);
+            var result = await soapRequestService.CreateSOAPRequest(request.Input);
+            return result;
+        }
+    }
+}
