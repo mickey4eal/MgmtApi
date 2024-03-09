9f46c0fa620f1654be262c92f7691889bce19bc1
Michael O <michaelobikwee@yahoo.co.uk>
2024-03-09 14:48:39 +0000

    Updated unit tests for SOAPRequestServiceFactory

--- ManagementApiTests/Factories/SOAPRequestServiceFactoryTests.cs
+++ ManagementApiTests/Factories/SOAPRequestServiceFactoryTests.cs
@@ -1,22 +1,25 @@
-﻿using AutoFixture.Kernel;
-using ManagementApi.Factories;
-using ManagementApi.Factories.Interfaces;
-using ManagementApi.Models;
-
-namespace ManagementApiTests.Factories
+﻿namespace ManagementApiTests.Factories
 {
+    using ManagementApi.Factories;
+    using ManagementApi.Factories.Interfaces;
+    using ManagementApi.Models;
+    using ManagementApi.Services;
+    using ManagementApi.Services.Interfaces;
+    using Moq;
+    using NSubstitute;
+    using NSubstitute.ExceptionExtensions;
+    using System.Data.SqlClient;
+
     public class SOAPRequestServiceFactoryTests : TestBase
     {
+        private readonly Mock<ISqlConnectionWrapper> _wrapperMock;
         private readonly ISOAPRequestServiceFactory _soapRequestServiceFactory;
         private const string CONNECTION_STRING = "Trusted_Connection=True;";
 
         public SOAPRequestServiceFactoryTests()
         {
-            _fixture.Customizations.Add(
-                new TypeRelay(
-                    typeof(ISOAPRequestServiceFactory),
-                    typeof(SOAPRequestServiceFactory)));
-            _soapRequestServiceFactory = _fixture.Create<ISOAPRequestServiceFactory>();
+            _wrapperMock = new Mock<ISqlConnectionWrapper>();
+            _soapRequestServiceFactory = new SOAPRequestServiceFactory(_wrapperMock.Object);
         }
 
         [Fact]
@@ -45,7 +48,48 @@ namespace ManagementApiTests.Factories
             var action = () => _soapRequestServiceFactory.Create(soapRequestServiceRequest);
 
             // Assert
-            Assert.Throws<ArgumentNullException>(() => action());
+            Assert.Throws<ArgumentException>(() => action());
         }
+
+        [Fact]
+        public void Create_ReturnsAuctionEventSOAPRequestService_ForSaleExecution()
+        {
+            // Arrange
+            var soapRequestServiceRequest = new SOAPRequestServiceRequest { ConnectionString = CONNECTION_STRING, ShouldExecuteForSale = true };
+
+            // Act
+            var soapRequestService = _soapRequestServiceFactory.Create(soapRequestServiceRequest);
+
+            // Assert
+            Assert.IsType<AuctionEventSOAPRequestService>(soapRequestService);
+        }
+
+        [Fact]
+        public void Create_ReturnsLotItemSOAPRequestService_ForNonSaleExecution()
+        {
+            // Arrange
+            var soapRequestServiceRequest = new SOAPRequestServiceRequest { ConnectionString = CONNECTION_STRING, ShouldExecuteForSale = false };
+
+            // Act
+            var soapRequestService = _soapRequestServiceFactory.Create(soapRequestServiceRequest);
+
+            // Assert
+            Assert.IsType<LotItemSOAPRequestService>(soapRequestService);
+        }
+
+        //[Fact]
+        //public void Create_Should_Throw_Exception_When__SoapRequestServiceFactory_Create_Method_Is_Called()
+        //{
+        //    // Arrange
+        //    var exception = new Exception();
+        //    var soapRequestServiceRequest = new SOAPRequestServiceRequest { ConnectionString = CONNECTION_STRING, ShouldExecuteForSale = false };
+        //    _soapRequestServiceFactory.Create(soapRequestServiceRequest).ThrowsForAnyArgs(exception);
+
+        //    // Act
+        //    var action = () => _soapRequestServiceFactory.Create(soapRequestServiceRequest);
+
+        //    // Assert
+        //    Assert.Throws<Exception>(() => action());
+        //}
     }
 }
